52d145f62ff6b71dac25d5118c5380979a16df2f
Azry <azry695@gmail.com>
2022-06-28 06:02:42 +0700

    test dulu

--- plugins/main-bot.js
+++ plugins/main-bot.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/5fc650798a93b835ef7e9.png', m, { packname: "rielbotz", author: "@riel" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/5fc650798a93b835ef7e9.png', m, packname, author })
 }
 
 handler.customPrefix = /^(bot|woi|Cok|ngab|tod|bang|hai)$/i
