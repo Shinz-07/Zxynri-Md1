3edc5f5f5030f10c644e33519ed383bc8aa9717a
Azry <azry695@gmail.com>
2022-06-28 05:45:36 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,13 +1,17 @@
-const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
-
-m.reply(wait)
-let p = await  hxz.ttdownloader(text)
-const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, '*DONE✅*', m)
+const { tiktokdl } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+*Nickname:* ${nickname}
+*Description:* ${description}
+`.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok|tiktoknowm)$/i
+
+handler.command = /^(tik(tok)?(dl)?)$/i
 
 module.exports = handler
