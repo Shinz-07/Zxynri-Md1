3785adcead73594abaafd3d4e15b8aa9559fd6ec
Azry <azry695@gmail.com>
2022-06-28 05:42:31 +0700

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -1,10 +1,12 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
+  try {
   let res = await fetch('https://api.waifu.pics/sfw/waifu')
-  if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
-  if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
+  conn.sendButtonImg(m.chat, json.url, 'Nihh waifunya', wm, `Next`, `${usedPrefix}${command}` , m)
+  } catch {
+    throw eror 
+  }
 }
 handler.help = ['waifu']
 handler.tags = ['anime']
